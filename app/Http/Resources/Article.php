50b9ece37955a6df5e77eac5b21c9e10a8cd4519
MD. Jahangir Alom Pial <jahangiralompial@gmail.com>
2018-10-05 21:57:35 +0600

    Backend API for Articles

---
+++ app/Http/Resources/Article.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Article extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        // return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'body' => $this->body
+        ];
+    }
+
+    public function with($request) {
+        return [
+            'version' => '0.0.1',
+            'author_url' => url('http://japial.github.io')
+        ];
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// List articles
+Route::get('articles', 'ArticleController@index');
+
+// List single article
+Route::get('article/{id}', 'ArticleController@show');
+
+// Create new article
+Route::post('article', 'ArticleController@store');
+
+// Update article
+Route::put('article/{id}', 'ArticleController@update');
+
+// Delete article
+Route::delete('article/{id}', 'ArticleController@destroy');
